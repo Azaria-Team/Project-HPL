51480bec0340dc57859cf3af00dc93ce2899c6d4
Slish <90574933+Slish562@users.noreply.github.com>
2023-11-05 21:53:58 +0200

    bugfix

--- src/hpl/content/HPLLoadouts.java
+++ src/hpl/content/HPLLoadouts.java
@@ -1,13 +1,32 @@
 package hpl.content;
 
+import mindustry.Vars;
 import mindustry.game.Schematic;
 import mindustry.game.Schematics;
+
+import java.io.IOException;
 
 public class HPLLoadouts{
     public static Schematic
             basicLegion;
 
     public static void load(){
-        basicLegion = Schematics.readBase64("bXNjaAF4nGNgZmBmZmDJS8xNZeBOzi9K1c1JTc/Mz2PgTkktTi7KLCgBcRgY2HISk1JzihmYomMZGfgzCnJ0kRUzMDCCECMDMwBMNhPL");
+       // basicLegion = Schematics.readBase64("bXNjaAF4nGNgZmBmZmDJS8xNZeBOSizOTPZJTc/Mz2PgTkktTi7KLCgBcRgY2HISk1JzihmYomMZGfgzCnJ0k/OLUnVzIIoZGBhBiJGBGQBPIhPX");
+        basicLegion = loadSchem("hpl-coreLegion");
     }
+
+        private static Schematic loadSchem(String name) {
+            Schematic s;
+            try {
+                s = Schematics.read(Vars.tree.get("schematics/" + name + ".msch"));
+            } catch (IOException e) {
+                s = null;
+                e.printStackTrace();
+            }
+            return s;
+        }
+
+        private static Schematic loadBase64(String b64) {
+            return Schematics.readBase64(b64);
+        }
 }
